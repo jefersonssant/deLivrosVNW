534efc6e75eed0b06965c15b5ced39cd6f3e5a6c
jefersonssant <jefss@outlook.com>
2024-11-25 19:30:22 -0300

    :sparkles: feat: Inclusão de nossa sessão na página Início e Footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import s from "./footer.module.scss"
+
+
+export default function Footer() {
+  return (
+    <footer className={s.footer}>
+      <h2>Eu sou a footer</h2>
+    </footer>
+  )
+}
--- src/pages/inicio/Inicio.jsx
+++ src/pages/inicio/Inicio.jsx
@@ -1,10 +1,35 @@
 import s from "./inicio.module.scss"
+import communityImg from "../../assets/1community.png"
+import readingImg from "../../assets/1reading.png"
+import transformImg from "../../assets/1transform.png"
+import balanceImg from "../../assets/1balance.png"
 
 export default function Inicio() {
-  return(
+  return (
     <main>
       <section className={s.primeiraSessaoInicio}>
         <h2>Venha fazer parte da maior rede de doação</h2>
+      </section>
+      <section className={s.porqueDoarSection}>
+        <h2>Porque devo doar?</h2>
+        <div className={s.containerCards}>
+          <section>
+            <img src={communityImg} alt="Imagem ilustrando circulo social de 4 pessoas" />
+            <p> Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
+          </section>
+          <section>
+            <img src={readingImg} alt="Imagem ilustrando pessoa lendo" />
+            <p>Estimula o hábito da leitura e o aprendizado contínuo.</p>
+          </section>
+          <section>
+            <img src={transformImg} alt="Imagem ilustrando 6 pessoas transformando vidas" />
+            <p>Fornece conhecimento e inspiração, permitindo que indivíduos transformem suas vidas.</p>
+          </section>
+          <section>
+            <img src={balanceImg} alt="Imagem ilustrando balança que representa igualdade" />
+            <p>Garante que todos, independentemente de sua condição, tenham oportunidades de aprendizado.</p>
+          </section>
+        </div>
       </section>
     </main>
   )
