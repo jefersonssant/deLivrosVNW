2917af30f03950d1466cf4c204a2684985814371
jefersonssant <jefss@outlook.com>
2024-11-27 19:49:27 -0300

    :sparkles: feat: Estilização de Footer e págins de Quero doar

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,27 @@
 import s from "./footer.module.scss"
+import facebook from "../../assets/facebook-app-round-white-icon 2.png"
+import instagram from "../../assets/instagram-white-icon 2.png"
+import linkedin from "../../assets/linkedin-icon-2048x2048-yx2cmt11 2.png"
+import twitter from "../../assets/Twitter_Logo_WhiteOnImage 2.png"
+import youtube from "../../assets/youtube-app-white-icon 2.png"
 
 
 export default function Footer() {
   return (
     <footer className={s.footer}>
-      <h2>Eu sou a footer</h2>
+      <section className={s.contatosFooter}>
+        <p>4002-8922</p>
+        <nav>
+          <a href=""><img src={facebook} alt="Imagem de ícone do Facebook" /></a>
+          <a href=""><img src={twitter} alt="Imagem de ícone do Twitter" /></a>
+          <a href=""><img src={youtube} alt="Imagem de ícone do Youtube" /></a>
+          <a href=""><img src={linkedin} alt="Imagem de ícone do LinkedIn" /></a>
+          <a href=""><img src={instagram} alt="Imagem de ícone do Instagram" /></a>
+        </nav>
+      </section>
+      <section className={s.copyright}>
+        <p>Layout desenvolvido pela Vai Na Web para fins educativos - 2024</p>
+      </section>
     </footer>
   )
 }
--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,5 +1,22 @@
+import s from "./queroDoar.module.scss"
+import livroTitle from "../../assets/Frame.png"
+
 export default function QueroDoar() {
   return (
-    <h2>Eu sou QueroDoar</h2>
+    <section className={s.queroDoarSection}>
+      <p>Por favor, preencha o formulário com suas informações e as informações do Livro</p>
+
+      <form action="">
+        <div>
+          <img src={livroTitle} alt="Imagem com ícone de livro com borda azul" />
+          <h2>Informações do Livro</h2>
+        </div>
+        <input type="text" name="" id="" placeholder="Título" />
+        <input type="text" name="" id="" placeholder="Categoria" />
+        <input type="text" name="" id="" placeholder="Autor" />
+        <input type="text" name="" id="" placeholder="Link da Imagem" />
+        <input type="submit" value="Doar" className={s.buttonDoar} />
+      </form>
+    </section>
   )
 }
