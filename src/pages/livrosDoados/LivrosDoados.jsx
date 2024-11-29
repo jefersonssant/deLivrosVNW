846522b46c0c85ea11f8a9ffece77ab98b4a5c86
jefersonssant <jefss@outlook.com>
2024-11-29 19:34:04 -0300

    :sparkles: feat: Página de livros doados e responsividade da header

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,5 +1,20 @@
+import livroOProtagonista from "../../assets/livroOProtagonista.png"
+import s from "./livrosDoados.module.scss"
+
 export default function LivrosDoados() {
   return (
-    <h2>Sou o componente LivrosDoados</h2>
+    <section className={s.livrosDoadosSection}>
+      <h2>Livros Doados</h2>
+      <section className={s.containerCards}>
+        <section>
+          <img src={livroOProtagonista} alt="Imagem do livro O protagonista" />
+          <div>
+            <h3>O protagonista</h3>
+            <p>Susanne Andrade</p>
+            <p>Ficção</p>
+          </div>
+        </section>
+      </section>
+    </section>
   )
 }
